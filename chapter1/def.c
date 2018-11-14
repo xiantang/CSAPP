d59120034e7789a1481ec22e7bbf45613683bb49
dd <986494553@qq.com>
2018-11-14 21:06:51 +0800

    修改了指针的代码

--- chapter1/def.c
+++ chapter1/def.c
@@ -11,6 +11,7 @@ void show_bytes(byte_pointer start,size_t len){
     printf("\n");
 }
 void show_int(int x){
+    // sizeof 来确定对象使用的字节数目
     show_bytes((byte_pointer)&x,sizeof(int));
 }
 
@@ -20,15 +21,25 @@ void show_float(float x){
 void show_pointer(void *x){
     show_bytes((byte_pointer)&x,sizeof(void *));
 }
+void test_show_bytes(int val){
+    int ival = val;
+    float fval = (float) ival;
+    int *pval = &ival;
+    show_int(ival);
+    show_float(fval);
+    // linux 64 使用了8字节地址表示指针
+    show_pointer(pval);
 
+}
 int main(int argc, char const *argv[])
 {
     /* code */
-    int c = 5231;
-    show_int(c);
-    float a = 123.12;
-    show_float(a);
-    void * b = show_float;
-    show_pointer(b);
-    return 0;
+    // int c = 5231;
+    // show_int(c);
+    // float a = 123.12;
+    // show_float(a);
+    // void * b = show_float;
+    // show_pointer(b);
+    // return 0;
+    test_show_bytes(12345);
 }
