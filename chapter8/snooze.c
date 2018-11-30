f9cb05639361ade13a81e176fabb5411471e8dd6
dd <986494553@qq.com>
2018-11-30 16:36:01 +0800

    添加csapp.c

--- chapter8/snooze.c
+++ chapter8/snooze.c
@@ -1,8 +1,21 @@
 #include "csapp.h" 
+unsigned int snooze(unsigned int secs){
+    unsigned int rc= sleep(secs);
+    printf("Slept %d of %d",secs-rc,secs);
+    return rc;
+}
 
-int main(int argc, char const *argv[])
+void handler(int sig){
+
+    return;
+}
+
+int main(int argc, char  *argv[])
 {   
-    printf("%s",argv[0]);
-    /* code */
-    return 0;
+    if (signal(SIGINT,handler)==SIG_ERR)
+        printf("signal error");
+    (void)snooze(argv[1]);
+    exit(0);
+
+
 }
